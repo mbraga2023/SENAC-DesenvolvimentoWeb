0472cc2987cff343a6300146b5cc58655a1cc224
Michel Braga <michel.diener@gmail.com>
2025-09-03 21:55:19 -0300

    aula 13 - chamadas api jQuery

--- Aulas/Aula 12/jquery/js/moedas.js
+++ Aulas/Aula 12/jquery/js/moedas.js
@@ -1,27 +1,66 @@
 $(document).ready(function () {
     const listaMoedas = $('#lista-moedas');
     const carregando = $('#loading');
+    const apiUrl = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?$top=100&$format=json'
+    const dataHoje = new Intl.DateTimeFormat('en-US').format(new Date());
+    console.log(dataHoje)
+
 
     async function buscarMoedas() {
-        try {
-            carregando.show();
-            listaMoedas.empty();
+        carregando.show()
+        $.getJSON(apiUrl).done(data => {
+            data.value.forEach(element => {
+                const simbolo = element.simbolo
+                const nome = element.nomeFormatado;
+                const cotacaoHoje = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='${simbolo}'&@dataCotacao='${dataHoje}'&$top=100&$format=json`
 
-            const response = await fetch('https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?$top=100&$format=json');
-            if (!response.ok) throw new Error('Erro ao buscar dados');
+                $.getJSON(cotacaoHoje).done(cotacao => {
+                    const venda = cotacao.value[4].cotacaoVenda
+                    const compra = cotacao.value[4].cotacaoCompra
 
-            const data = await response.json();
+                    const vendaFormatada = venda.toLocaleString('pr-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    })
+                    const compraFormatada = compra.toLocaleString('pr-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    })
 
-            data.value.forEach(moeda => {
-                const item = `<li>${moeda.simbolo} - ${moeda.nomeFormatado}</li>`;
-                listaMoedas.append(item);
+                    const item = `<li class='moeda-item'>
+                <span class='simbolo'>${simbolo}</span> 
+                ${nome}  
+                <span class='valorCompraVenda'> 
+                Compra ${compraFormatada} - 
+                Venda ${vendaFormatada} </span>
+                </li>`;
+
+                    listaMoedas.append(item);
+                })
             });
-        } catch (error) {
-            listaMoedas.append('<li>Erro ao carregar moedas.</li>');
-            console.error(error);
-        } finally {
-            carregando.hide();
-        }
+        })
+        carregando.hide()
+
+
+        // try {
+        //     carregando.show();
+        //     listaMoedas.empty();
+
+        //     const response = await fetch('https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?$top=100&$format=json');
+        //     if (!response.ok) throw new Error('Erro ao buscar dados');
+
+        //     const data = await response.json();
+
+        //     data.value.forEach(moeda => {
+        //         const item = `<li>${moeda.simbolo} - ${moeda.nomeFormatado}</li>`;
+        //         listaMoedas.append(item);
+        //     });
+        // } catch (error) {
+        //     listaMoedas.append('<li>Erro ao carregar moedas.</li>');
+        //     console.error(error);
+        // } finally {
+        //     carregando.hide();
+        // }
     }
 
     buscarMoedas();
